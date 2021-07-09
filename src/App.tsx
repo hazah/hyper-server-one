6905c2b868a8741ad19c1df84f65d4981b64cf18
Ivgeni Slabkovski <ivgenis@gmail.com>
2021-07-08 22:54:26 -0400

    screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '@fontsource/roboto';
 
-import Home from 'pages/Home';
-import About from 'pages/About';
+import Home from 'screens/Home';
+import About from 'screens/About';
 
 import Navigation from 'Navigation';
 
