bc579b215dedc4d1a1eac1ec1343c0dd7552f9ba
Ivgeni Slabkovski <ivgenis@gmail.com>
2021-12-04 13:26:23 -0500

    server application wrapper

--- lib/infra/http/renderer.tsx
+++ lib/infra/http/renderer.tsx
@@ -1,18 +1,25 @@
-import fs from "fs";
-import React from "react";
+import React, { Component as ReactComponent } from "react";
 import { renderToStaticMarkup, renderToString } from "react-dom/server";
+
+function wrap(Component: ReactComponent): ReactComponent {
+  return Component;
+}
 
 export default async function jsxEngine(path: string, options: any, callback: (e: any, rendered?: string) => void): Promise<void> {
   try {
     const Component = require(`@app/${path.substring(0, path.length - 4).split("/app/")[1]}`).default;
     const isStatic = !!options.static;
-
+    const isApp = !!options.app;
+    
     delete options.static;
+    delete options.app;
+
+    const PreparedComponent = isApp ? wrap(Component) : Component;
 
     callback(
       null,
-      isStatic  ? renderToStaticMarkup(<Component {...options}/>) 
-                : renderToString(<Component {...options}/>)
+      isStatic  ? renderToStaticMarkup(<PreparedComponent {...options}/>) 
+                : renderToString(<PreparedComponent {...options}/>)
     );
   } catch (error) {
     callback(error);
