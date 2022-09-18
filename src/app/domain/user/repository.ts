6bb57b1ddb9052a5d48bd475e98b87723af70d33
Ivgeni Slabkovski <ivgenis@gmail.com>
2022-09-17 20:52:55 -0400

    type edit

--- src/app/domain/user/repository.ts
+++ src/app/domain/user/repository.ts
@@ -1,10 +1,11 @@
+import UniqueEntityID from "@domain/unique_entity_id";
 import User from "@app/domain/user";
 import Email from "@app/domain/user/email";
 import Name from "@app/domain/user/name";
 
 export default interface Repository {
-  exists(email: Email): Promise<boolean>;
-  getById(id: string): Promise<User>;
+  exists(email: Email | string): Promise<boolean>;
+  getById(id: UniqueEntityID | string): Promise<User>;
   getByName(name: Name | string): Promise<User>;
   save(user: User): Promise<void>;
 }
